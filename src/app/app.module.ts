3e90656afbf8608be72f1603d7996546634b2c45
unknown <manishachauhan79100@gmail.com>
2022-12-26 19:29:23 +0530

    banner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { ContactComponent } from './pages/contact/contact.component';
 import { BannerComponent } from './layout/banner/banner.component';
 import { ProductListComponent } from './layout/product-list/product-list.component';
 import { AddComponent } from './layout/add/add.component';
+import { LeftSidebarComponent } from './layout/left-sidebar/left-sidebar.component';
 
 
 @NgModule({
@@ -33,6 +34,7 @@ import { AddComponent } from './layout/add/add.component';
     BannerComponent,
     ProductListComponent,
     AddComponent,
+    LeftSidebarComponent,
   ],
   imports: [
     BrowserModule,
